f5c24be73be03c89a5444d25e70c72b07ad07ff8
Jason Daniels <jason.c.daniels@gmail.com>
2021-10-08 17:33:23 -0500

    Initial commit after breaking off from Jcd.Utilities

---
+++ Jcd.Reflection.Tests/TestHelpers/NegativeNaiiveFibonacciGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+namespace Jcd.Reflection.Tests.TestHelpers
+{
+   /// <summary>
+   ///    Generates all Fibonacci numbers equal to or greater than the minValue
+   /// </summary>
+   public class NegativeNaiiveFibonacciGenerator :
+      CaptureAndTransitionGenerator<NegativeNaiiveFibonacciGenerator.State, BigInteger>
+   {
+      public NegativeNaiiveFibonacciGenerator(BigInteger minValue)
+         : base(new State {N0 = 0, N1 = -1, Nth = 0},
+                (State state, out bool @continue) =>
+                {
+                   var t = state.N0;
+                   state.N0 = state.N1;
+                   state.N1 += t;
+                   state.Nth++;
+                   @continue = state.N0 >= minValue;
+
+                   return state.N1 - state.N0;
+                })
+      {
+         CurrentState.Nth = 0;
+      }
+
+      public class State
+      {
+         public BigInteger N0;
+         public BigInteger N1;
+         public int Nth;
+      }
+   }
+}
---
+++ Jcd.Reflection.Tests/TestHelpers/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Jcd.Reflection.Tests.TestHelpers
+{
+   /// <summary>
+   ///    A validation helper. BigInteger formats hex numbers inconsistent from other int types (Zero Pads)
+   /// </summary>
+   public static class StringExtensions
+   {
+      /// <summary>
+      ///    Removes leading zeros from a given bit of text.
+      /// </summary>
+      /// <param name="text">The text to remove leading zeros from.</param>
+      public static string TrimLeadingZeros(this string text)
+      {
+         var sb = new StringBuilder();
+         var isLeadingZero = true;
+
+         foreach (var c in text)
+         {
+            isLeadingZero = isLeadingZero && (c == '0');
+
+            if (!isLeadingZero) sb.Append(c);
+         }
+
+         if ((sb.Length == 0) &&
+             (text.Length > 0)) sb.Append('0');
+
+         return sb.ToString();
+      }
+   }
+}
